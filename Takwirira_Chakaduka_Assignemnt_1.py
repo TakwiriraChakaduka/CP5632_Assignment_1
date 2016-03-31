9418a197a7b13fa1d80d56a03d55911fb05dbe64
MY-PC <sheengy24@gmail.com>
2016-03-31 09:10:38 -0700

    Initial commit

---
+++ Takwirira_Chakaduka_Assignemnt_1.py
@@ -0,0 +1,23 @@
+"""function main()
+	load items from file
+	display welcome msg
+	display menu
+	get choice
+	while choice not ‘q’
+		if choice is ‘l’
+			call load items()
+		else if choice is ‘h’
+			call item hire()
+		else if choice is ‘r’
+			return items
+			display item returned
+		else
+			display error message (invalid choice)
+		display menu
+		get choice
+	print farewell message
+
+load items ()
+myfile = open(“items.csv”,’r’)
+data = myfile.read()
+print(data)"""
