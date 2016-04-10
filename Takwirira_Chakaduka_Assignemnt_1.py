6c99a53994e99ccc98513b9f3528fe00e3c591a5
MY-PC <sheengy24@gmail.com>
2016-04-10 15:36:13 -0700

    updated loading, hiring, return functions

--- Takwirira_Chakaduka_Assignemnt_1.py
+++ Takwirira_Chakaduka_Assignemnt_1.py
@@ -31,29 +31,11 @@ def main():
     choice = input(">>> ").upper()
     while choice != 'Q':
         if choice == "L":
-            load_items()
+            loading_items()
         elif choice == "H":
             hire_items()
         elif choice == "R":
-            items_list = []
-            item_name = input("Enter the name of the item")
-            if item_name == " ":
-                print("please enter name of item")
-            else:
-                items_list.append(item_name)
-                break
-            item_description = input("Description: ")
-            if item_description == " ":
-                print("Please enter description")
-            else:
-                items_list.append(item_description)
-                break
-            item_charges = float(input("price per day: "))
-            if item_charges == "" or item_charges <=0:
-                print("Please enter valid amount")
-            else:
-                items_list.append(item_charges)
-                break
+            return_items()
         elif choice == "A":
             items_list = []
             item_name = input("Enter the name of the item")
@@ -76,7 +58,6 @@ def main():
                 break
         else:
             print("Invalid menu choice.")
-
         print(Menu)
         choice = input(">>> ")
 
@@ -84,22 +65,85 @@ def main():
 
 
 
+def loading_items():
+    print("All items  on file ( '*' indicates item is currently out):")
+    items = load()
+    for key in sorted(items.keys()):
+        item = items[key]
+        itemstatus = item[-1]
+        if itemstatus == "out":
+            string_tail = "*"
+        else:
+            string_tail = ""
+        name = item[0]
+        print(str(key) + "-" + name + "(" + item[1] + ")" + "\t=" + "\t$ " + item[2] + string_tail)
 
 
+def save(items):
+    with open("items.csv", "w") as fd:
+        for x in range(len(items)):
+            record = items[x]
+            fd.write(','.join(record) + "\n")
+def load():
+    items = {}
 
-"""This function will load items from file"""
-def load_items():
-    items_file = open("items.csv","r")
-    count = 0
-    items_file.seek()
-    for row in items_file:
-        print (row)
+    with open("items.csv", "r") as fd:
+        for idx, line in enumerate(fd.readlines()):
+            record = line.split(',')
+            record = [ value.rstrip() for value in record ]
+            items[idx] = record
 
+    return items
+
+def hire_items():
+    items = load()
+    item_index = get_user_value("Enter the number of item to hire", len(items))
+    item_line = items[item_index]
+    item_properties = item_line
+    print( " ".join(item_properties) + " hired ")
+    record = items[item_index]
+    if record[-1] == 'out':
+        print( " That item is already been hired")
+        return
+
+    record[-1] = 'out'
+    items[item_index] = record
+    save(items)
+
+def items_that_are_out(items):
+    result = {}
+    for k,i in items.items():
+        if i[-1] == 'out':
+            result[k] = i
+    return result
+
+def get_user_value(msg, max_num):
+    while True:
+        try:
+            value = input(msg)
+            return int(value)
+        except:
+            print("Invalid number. Please enter a number between 0 and %d" % max_num)
+
+def return_items():
+    items = load()
+
+    items_out = items_that_are_out(items)
+
+    for key in sorted(items_out.keys()):
+        print(key, " ".join(items_out[key]))
+
+    item_index = get_user_value("Enter the number of item to return", len(items))
+    item_line = items_out[item_index]
+    print( " ".join(item_line) + " returned ")
+    record = items_out[item_index]
+    if record[-1] == 'in':
+        print( " That item is already been returned")
+        return
+
+    record[-1] = 'in'
+    items[item_index] = record
+    save(items)
 
 
 main()
-
-
-
-def hire_items():
-    print("Hire items")
